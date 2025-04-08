551ad6afeb3fc4db6dfda363a378a3dcffa96b6f
harry <harishmetagudd12@gmail.com>
2025-04-08 12:03:49 +0530

    sum.java

---
+++ sum.java
@@ -0,0 +1,10 @@
+package java;
+
+public class sum {
+    public static void main(String[] args){
+        int a=12,b=10;
+         System.out.println("Addition of a and b="+(a+b));
+         System.out.println("Substraction of a and b="+(a-b));
+    }
+
+}
