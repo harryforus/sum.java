2dd0b950204973d290ea26b9436b834889619393
harry <harishmetagudd12@gmail.com>
2025-04-08 12:17:54 +0530

    This is in master

--- sum.java
+++ sum.java
@@ -5,6 +5,7 @@ public class sum {
         int a=12,b=10;
          System.out.println("Addition of a and b="+(a+b));
          System.out.println("Substraction of a and b="+(a-b));
+         System.out.println("you are in master");
     }
 
 }
